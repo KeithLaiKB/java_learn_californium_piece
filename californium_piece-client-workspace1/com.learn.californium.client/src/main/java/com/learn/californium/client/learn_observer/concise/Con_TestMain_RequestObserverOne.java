a1521aad7ef8127013c8a1a38eea80fc4e6fccbd
KeithLaiKB <laidelivery@163.com>
2021-05-10 15:40:23 +0800

    update minimal working example

---
+++ californium_piece-client-workspace1/com.learn.californium.client/src/main/java/com/learn/californium/client/learn_observer/concise/Con_TestMain_RequestObserverOne.java
@@ -0,0 +1,138 @@
+package com.learn.californium.client.learn_observer.concise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.CoapObserveRelation;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.CoAP.Code;
+import org.eclipse.californium.core.coap.CoAP.Type;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.elements.exception.ConnectorException;
+/**
+ * 
+ * @author laipl
+ *
+ * 参考了 californium/demo-apps/cf-plugtest-client/src/main/java/org/eclipse/californium/plugtests/PlugtestClient.java
+ */
+public class Con_TestMain_RequestObserverOne {
+    public static void main(String[] args) {
+
+    	String myuri1 	     = "coap://localhost:5656/hello_observer";
+    	//
+    	//
+    	CoapClient client = new CoapClient(myuri1);
+        //
+        CoapObserveRelation coapObRelation1;
+		//
+        try {
+			
+        	// set handler for observer method, because observe method needs asynchronous operation
+			CoapHandler myObserveHandler = new CoapHandler() {
+
+	            @Override
+	            public void onLoad(CoapResponse response) {
+	                //log.info("Command Response Ack: {}, {}", response.getCode(), response.getResponseText());
+	            	System.out.println("---------------------------------------");
+	            	System.out.println("on load: " + response.getResponseText());
+	            	System.out.println("get code: " + response.getCode().name());
+	            	
+	            }
+
+	            @Override
+	            public void onError() {
+	            }
+	        };
+	        //
+	        //
+	        coapObRelation1 = client.observe(myObserveHandler);
+	        //
+	        System.out.println("wow_hello");
+	        //
+	        if (coapObRelation1!=null) {
+	        
+	        	System.out.println( "response getCode:");
+	        	System.out.println( "response getOptions:");
+	        	//System.out.println( "response text:" + response.toString() );
+	        	//System.out.println( "payload:" + response.getCurrent().getResponseText() );
+	        	//response.getCurrentResponse();
+	        	//System.out.println( "payload:" + new String(response.getCurrent().getPayload()) );
+	        	
+	        	//System.out.println(xml);
+	        } else {
+	        	
+	        	System.out.println("Request failed");
+	        	
+	        }	
+	        //
+	        //
+	        //
+	        Scanner in =new Scanner(System.in) ;
+            int int_choice = 0;
+            while(int_choice!=-1) {
+            	System.out.println("here is the choice:");
+            	System.out.println("-1: to exit");
+            	System.out.println("1: to delete");
+            	System.out.println("2: to reactiveCancel");
+            	System.out.println("3: to proactiveCancel");
+            	System.out.println("4: to observe again");
+            	System.out.println("enter the choice:");
+            	// input
+            	int_choice = in.nextInt();
+            	if(int_choice==-1) {
+            		//System.exit(0);
+            		break;
+            	}
+            	else if(int_choice==1) {
+            		//
+            		System.out.println("deleteing record");
+            		//System.out.println("deleting resources");
+            		//
+            		// 我认为 delete 挺重要的 所以我这选择的是同步
+            		client.delete();				// 用的是 同步, 对面没回应, 就不能继续往下走
+            		//client.delete(myDeleteHandler); // 用的是 异步
+            	}
+            	else if(int_choice==2) {
+            		coapObRelation1.reactiveCancel();				//取消观察状态
+            		System.out.println("reactiveCancel");
+            		
+            	}
+            	else if(int_choice==3) {
+            		coapObRelation1.proactiveCancel();				//取消观察状态
+            		System.out.println("proactiveCancel");
+            	}
+            	else if(int_choice==4) {
+            		coapObRelation1 = client.observe(myObserveHandler); //取消观察状态后 还是可以继续observe的
+            		System.out.println("observe again");
+            	}
+            }
+	        
+	        
+            in.close();
+            
+			//String xml = client.get(MediaTypeRegistry.APPLICATION_XML).getResponseText();
+			//response.reactiveCancel();
+            /*
+	        System.out.println("enter to exit!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			try { br.readLine(); } catch (IOException e) { }
+			System.out.println("CANCELLATIONING");
+			response.proactiveCancel();
+			System.out.println("CANCELLATION FINISHED");
+			*/
+            client.shutdown();
+            System.exit(0);
+		//		
+		//	
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
+}
