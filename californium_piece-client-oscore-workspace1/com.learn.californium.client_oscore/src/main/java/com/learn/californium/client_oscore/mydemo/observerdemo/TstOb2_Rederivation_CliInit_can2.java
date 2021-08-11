0bef29ebdad1140e24706188fbc7428b759e2fbb
KeithLaiKB <laidelivery@163.com>
2021-08-12 00:03:52 +0800

    update something

--- californium_piece-client-oscore-workspace1/com.learn.californium.client_oscore/src/main/java/com/learn/californium/client_oscore/mydemo/observerdemo/TstOb2_Rederivation_CliInit_can2.java
+++ californium_piece-client-oscore-workspace1/com.learn.californium.client_oscore/src/main/java/com/learn/californium/client_oscore/mydemo/observerdemo/TstOb2_Rederivation_CliInit_can2.java
@@ -101,7 +101,7 @@ public class TstOb2_Rederivation_CliInit_can2 {
 			//
 			//OSCoreCtx ctx = new OSCoreCtx(master_secret, true, alg, sid, rid, kdf, 0, master_salt, null);
 			//OSCoreCtx ctx = new OSCoreCtx(master_secret, true);
-			OSCoreCtx ctx = new OSCoreCtx(master_secret, true, alg, sid, rid, kdf, 32, master_salt, myContextId1);
+			OSCoreCtx ctx = new OSCoreCtx(master_secret, true, alg, sid, rid, kdf, 32, master_salt, null);
 			//db.addContext("coap://" + "127.0.0.1", ctx);
 			//db.addContext("coap://" + uri_addr2, ctx);
 			//db.addContext(uriLocal, ctx);
