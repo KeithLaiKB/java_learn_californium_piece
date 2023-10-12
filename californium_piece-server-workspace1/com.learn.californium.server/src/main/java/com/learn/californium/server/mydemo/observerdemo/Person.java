f59c09aba6ee68d94f1228528039e81ea8b562fa
KeithLaiKB <laidelivery@163.com>
2023-10-12 18:18:46 -0400

    update demo

---
+++ californium_piece-server-workspace1/com.learn.californium.server/src/main/java/com/learn/californium/server/mydemo/observerdemo/Person.java
@@ -0,0 +1,26 @@
+package com.learn.californium.server.mydemo.observerdemo;
+/**
+ * 
+ * could be deleted
+ * 
+ * <p>
+ * description:				</br>
+ *
+ * </p>
+ *
+ *
+ *
+ * @author laipl
+ *
+ * could be deleted, 只是拿来测试 类中的类 是否会影响到LCOM CBO 等等之类的数值而已
+ */
+public class Person{
+	
+	
+	public class Leg {
+		int legnum=2;
+		public class Hair{
+			int hairnum=1000;
+		}
+    }
+}
